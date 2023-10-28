b55fa5ffb87b12447368eaff59bc4f9be88b20f9
Ayush Kumar <ayushkumar121@outlook.com>
2023-10-28 15:12:09 +0530

    Updating algorithm that better conform to requirement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
+
 export class AppComponent {
   error = '';
   seatCount = 0;
   seatsBooked = 0;
-  seats = new Uint8Array(80);
-  recentlyBooked: number[] = [];
+  coach = new Coach(80, 7);
+  //recentlyBooked: number[] = [];
 
-  constructor() {
-    this.bookSeats(6);
+  constructor() {	
+      this.bookSeats(5);
+      this.bookSeats(7);
+      this.bookSeats(6);
+      this.bookSeats(7);
+      this.bookSeats(7);
+      this.bookSeats(7);
+      this.bookSeats(7);
+      this.bookSeats(7);
+      this.bookSeats(5);
+      this.bookSeats(5);
+      this.bookSeats(3);
   }
 
   setError(newError: string) {
@@ -25,8 +36,16 @@ export class AppComponent {
     this.error = '';
   }
 
-  isRecentBooking(index: number) {
-    return this.recentlyBooked.findIndex((x) => x === index) != -1;
+  /* 
+  * Given rows this funtion returs the sum of 
+  * distances between them
+  * */
+  getRowCost(rows: number[]): number {
+      let cost = 0;
+      for(let i=0; i<rows.length-1; i++) {
+          cost += rows[i+1]-rows[i];
+      }
+      return cost;
   }
 
   /*
@@ -35,38 +54,122 @@ export class AppComponent {
   also returns the seat numbers of seat
   booked
   */
-  bookSeats(seatCount: number) {
+  bookSeats(seatCount: number): number[] {
     this.resetError();
 
      if (seatCount < 1 || seatCount > 7) {
       this.setError('Cannot book more than 7 and less 1 seat at a time');
-      return;
+      return [];
     }
 
-    if (seatCount + this.seatsBooked > this.seats.length) {
-      this.setError('Cannot book more than coach capaity');
-      return;
+     if (this.seatsBooked + seatCount > this.coach.totalSeatCount) {
+      this.setError('Not enought seats are available');
+      return [];
     }
 
-    let seatNumbers: number[] = [];
-    for (let i = 0; i < seatCount; i++) {
-      let k = this.seatsBooked;
-      let row = Math.floor(k / 7) + 1;
+    /* Find seats in a row*/
+    
+    let bestRow = -1;
+    let minCost = Number.POSITIVE_INFINITY;
 
-      if (row % 2 == 0) {
-        k = 7 * row - (1 + (this.seatsBooked % 7));
-      }
-
-      if (k > 79) {
-          k -= 4;
-      }
-          
-      this.seats[k] = 1;
-      this.seatsBooked++;
-
-      seatNumbers.push(k);
+    for (let i=0; i<this.coach.rows.length; i++) {
+	let row = this.coach.rows[i];
+	let cost = row.emptySeats - seatCount;
+	
+	if (cost >= 0 && cost < minCost) {
+    		minCost = cost;
+    		bestRow = i;
+	}
     }
 
-    this.recentlyBooked = seatNumbers;
+    /* 
+    * Filling up in base case ie. we have a single
+    * row free 
+    * */
+    if (bestRow != -1) {
+        this.seatsBooked += seatCount;
+    	this.coach.rows[bestRow].emptySeats -= seatCount;
+    	return [bestRow];
+    }
+
+   /* 
+   * Filling up seats incase we don't have 
+   * seats available in a row 
+   * */
+
+    let bookedSoFar = 0;
+    let rows: number[] = [];
+    let bestRows: number[] = [];
+    
+    minCost = Number.POSITIVE_INFINITY;
+
+    for (let i=0; i<this.coach.rows.length; i++) {
+	let row = this.coach.rows[i];
+	if (row.emptySeats == 0) continue;
+
+	rows.push(i);
+	bookedSoFar += row.emptySeats;
+
+	if (bookedSoFar >= seatCount) {
+    		let cost = this.getRowCost(rows);
+		if (cost < minCost) {
+    			bestRows = rows;
+    			minCost = cost;
+		}
+
+    		// Reseting the state so we can try another case
+		rows = [];
+    		bookedSoFar = 0;
+	}
+    }
+    
+
+    bookedSoFar = 0;
+    for (let i=0; i<bestRows.length; i++) {
+        let rowIndex = bestRows[i];
+        let row = this.coach.rows[rowIndex];
+        let toBook = Math.min(seatCount-bookedSoFar, row.emptySeats);
+        row.emptySeats -= toBook;
+        bookedSoFar += toBook;
+    }
+   
+    return bestRows;
   }
 }
+
+interface Row {
+    totalSeats: number; 
+    /* 
+    * Number of empty seats in the row
+    * The way we're filling seats makes
+    * sures only the last seats in a row
+    * are empty.
+    */ 
+    emptySeats: number;
+}
+
+class Coach {
+    rows : Row[];
+    totalSeatCount: number;
+
+    /*
+    * @totalSeatCount: Number of total seats in a coach
+    * @seatsInRow: Number of seats in a row
+    */
+    constructor(totalSeatCount: number, seatsInRow: number) {
+        let rowCount = Math.floor(totalSeatCount/seatsInRow);
+        let lastRowSeats = totalSeatCount%seatsInRow;
+
+	this.rows = [];
+	this.totalSeatCount = totalSeatCount;
+	
+       	/* Fill up 1st to row-1 rows*/
+	for (let i=0; i<rowCount-1; i++) {
+    		this.rows.push({totalSeats: seatsInRow, emptySeats: seatsInRow});
+	}
+
+	/* Fill up last row*/
+	this.rows.push({totalSeats: lastRowSeats, emptySeats: lastRowSeats});
+    } 
+}
+
